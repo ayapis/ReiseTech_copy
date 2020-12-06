ebae93f743eed9ae28ff855c4ec31cabd2e7acff
ayapis <ayapisss@gmail.com>
2020-12-07 00:01:16 +0900

    ハンバーガーメニュー作成中です。

--- raisetechcopy.js
+++ raisetechcopy.js
@@ -1,6 +1,10 @@
 $(function(){
-  const ham = document.getElementById('ham');
-  ham.addEventListener('click', function() {
-	alert('test');
-});
+//   const ham = document.getElementById('ham');
+//   ham.addEventListener('click', function() {
+// 	alert('test');
+// });
+
+  $('#ham').on('click', function(){
+    $('.p-gnavi').toggleClass('u-forwide p-gnavi-ham');
+  });
 });
