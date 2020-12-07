ab4e9638078218dc5efe94b21cc2d20bf4554a29
ayapis <ayapisss@gmail.com>
2020-12-07 20:07:42 +0900

    ナビゲーションメニューの動きを実装中です。

--- raisetechcopy.js
+++ raisetechcopy.js
@@ -1,10 +1,5 @@
 $(function(){
-//   const ham = document.getElementById('ham');
-//   ham.addEventListener('click', function() {
-// 	alert('test');
-// });
-
   $('#ham').on('click', function(){
-    $('.p-gnavi').toggleClass('u-forwide p-gnavi-ham');
+    $('#navi').toggleClass('p-gnavi-ham').slideToggle('p-gnavi-ham');
   });
 });
