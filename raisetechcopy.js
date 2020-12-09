d2a30edec497149072fb76512793684777e4b5c8
ayapis <ayapisss@gmail.com>
2020-12-10 00:15:32 +0900

    ナビゲーションメニューの修正を行いました。（last-of-typeが予期せぬ箇所にあたっていた部分）

--- raisetechcopy.js
+++ raisetechcopy.js
@@ -4,13 +4,21 @@ $(function(){
     var x = 1200;
     if (w >= x) {
 //画面サイズが1200px以上のときの処理
-      $(".p-gnavi").removeClass(".p-gnavi-ham");
+      $(".p-gnavi").removeClass("p-gnavi-ham");
     } else {
 //それ以外のときの処理
-      $(".p-gnavi").addClass(".p-gnavi-ham");
+      $(".p-gnavi").addClass("p-gnavi-ham");
     }
   });
   $('#ham').on('click', function(){
     $('#navi').slideToggle('p-gnavi-ham');
   });
+  $(".p-gnavi__list__item").hover(
+    function() {
+      $(".p-gnavi__subList", this).slideDown(200);
+    },
+    function() {
+      $(".p-gnavi__subList", this).slideUp(100);
+    }
+  );
 });
